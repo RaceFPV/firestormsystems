25b73b754fe6c7d08d211c94e1e263b317cf7fa9
slacker87 <racethesunlive@yahoo.com>
2014-02-19 03:50:14 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firestormsystems::Application.config.secret_key_base = 'be768bca79232d52cd502ced03ead8e1266ae13ecc05ffb82ff2fb1099e81e055a5a7642487083723ca9af0e6ba4fa62ca0d4713e7dc274498d48282ebb79ad5'
